39dffca51fad28c50a19bd4acbec9e7bcf761d85
Daniel Yu <dcyu93@gmail.com>
2013-10-05 11:27:37 -0400

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BotherCongress::Application.config.secret_token = 'aadc1752002c9bee753f5aee010190dab2a9c9a51c59d7abe56c0033112498a51cff21ed3ee00b95c4788a089e2907e38da9d29491c9b15925c84b8eb24e96c6'
